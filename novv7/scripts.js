97461483baae108d6fa189aef690e2f8050b856e
Pedro Felipe Verissimo Stumb <v375376@gmail.com>
2022-12-25 18:02:42 -0300

    New!

---
+++ novv7/scripts.js
@@ -0,0 +1,13 @@
+let comercial = window.document.getElementById('comercial');
+
+function entrou() //onmouseenter
+{
+    comercial.style.backgroundColor = 'black';
+    comercial.style.color = 'white';
+}
+
+function saiu() //onmouseout
+{
+    comercial.style.backgroundColor = 'lightray';
+    comercial.style.color = 'black';
+}
