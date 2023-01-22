0f033477ca2856d1d59ec0ea18459e51a92329a7
Pedro Felipe Verissimo Stumb <v375376@gmail.com>
2023-01-22 00:35:36 -0300

    New!

---
+++ novv9/rebozinho.py
@@ -0,0 +1,9 @@
+import keyboard
+
+valor1 = 11
+valor2 = 90
+
+result = valor1 + valor2
+
+keyboard.write('{} é o resultado de minha conta feita diretamento em python' result, delay=0.2)
+keyboard.press('enter')
